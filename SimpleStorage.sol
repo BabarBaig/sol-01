e3c2c302bf92cbb9e3b2765d7a70c6c6c3cae4cd
Babar Baig <BobBaig@outlook.com>
2022-03-06 10:16:48 -0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.1;
 
+/**
+ * @title FirstContract
+ * @dev Store & retrieve value in a variable
+ */
 contract FirstContract {
     uint var1;
 
+    /**
+     * @dev Set value in variable
+     * @param num value to store
+     */
     function set(uint x) public {
         var1 = x;
     }
 
+    /**
+     * @dev Get value 
+     * @return value of 'var1'
+     */
     function get() public view returns (uint) {
         return var1;
     }
